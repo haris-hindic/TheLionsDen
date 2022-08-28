d5448878bbd63f8078ddce8ea1643476c1bb7c5d
Haris Hindić <haris.hindic@edu.fit.ba>
2022-08-28 15:59:42 +0200

    Change port

--- the_lions_den_mobile/lib/providers/base_provider.dart
+++ the_lions_den_mobile/lib/providers/base_provider.dart
@@ -17,7 +17,7 @@ abstract class BaseProvider<T> with ChangeNotifier {
 
   BaseProvider(String endPoint) {
     _baseUrl = const String.fromEnvironment("baseUrl",
-        defaultValue: "https://10.0.2.2:7070/");
+        defaultValue: "http://10.0.2.2:5070/");
 
     if (_baseUrl!.endsWith("/") == false) {
       _baseUrl = _baseUrl! + "/";
